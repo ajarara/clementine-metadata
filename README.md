2a50fb88fbfd494b1eb0a9f07dfa248f42939ca4
Ahmad Jarara <ajarara@jarmac.org>
2015-12-30 14:15:59 -0500

    added the README, included protocol option

---
+++ README.md
@@ -0,0 +1,6 @@
+Invoke on the shell with
+```
+$ python3 clem.py
+```
+
+If you listen to internet radio, you might find it useful to uncomment the protocol line. This'll tell you what radio you were listening to, in most cases.
--- clem.py
+++ clem.py
@@ -4,4 +4,6 @@ player = session_bus.get_object('org.mpris.clementine', '/Player')
 iface = dbus.Interface(player, dbus_interface='org.freedesktop.MediaPlayer')
 metadata = iface.GetMetadata()
 
+#protocol = metadata['location'] + '\n'
+
 print(metadata['title'] + ' - ' + metadata['artist'])
